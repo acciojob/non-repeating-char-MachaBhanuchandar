31861e62b1326d22604952422093034a43bbdfe7
MACHA BHANUCHANDAR <114289114+MachaBhanuchandar@users.noreply.github.com>
2025-05-16 11:34:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const charCount={};
+	for (let i = 0; i < str.length; i++) {
+		const char=str[i];
+		charCount[char]=(charCount[char]||0)+1;
+	}
+	for (let j = 0; j < str.length; j++) {
+		if(charCount[str[j]]===1){
+			return str[j];
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
